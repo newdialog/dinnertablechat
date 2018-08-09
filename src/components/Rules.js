e47f163ab3907d092bdc3ab65203511cb8fb51b6
billie-badger <will.vatcher@gmail.com>
2018-08-09 15:44:39 -0700

    Initial Commit
    Former-commit-id: 1c18b6ecdb57c4f83e6107f1a15708cec9e60b76

---
+++ src/components/Rules.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Rules extends Component {
+    render() {
+        return (
+            <div id='rules'>
+                
+            </div>
+        );
+    }
+}
+
+export default Rules;
