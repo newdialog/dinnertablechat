05be34bbba8cf9b1b3111c9fc1260feb125762f5
Jonathan Dunlap <jdunlap@outlook.com>
2018-08-14 13:22:28 -0700

    banner added
    Former-commit-id: 328d24cbc521facb9f90898c0dbdd169da059c52

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,105 +12,128 @@ import withRoot from '../withRoot';
 import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
+// import bannerImg from '../../public/assets/banner2.jpg'
+// const bannerImg = require('../assets/banner2.jpg')
 
-const styles = (theme: any) => createStyles({
-  root: {
-    textAlign: 'center',
-    paddingTop: theme.spacing.unit * 20,
-  },
-  container: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(12, 1fr)',
-    gridGap: `${theme.spacing.unit * 3}px`
-    // gridAutoFlow: 'column',
-    // gridAutoColumns: '200px'
-  },
-  paper: {
-    padding: theme.spacing.unit,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    whiteSpace: 'nowrap',
-    marginBottom: theme.spacing.unit,
+const styles = (theme: any) =>
+  createStyles({
+    root: {
+      textAlign: 'center',
+      paddingTop: theme.spacing.unit * 20
+    },
+    container: {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(12, 1fr)',
+      gridGap: `${theme.spacing.unit * 3}px`
+      // gridAutoFlow: 'column',
+      // gridAutoColumns: '200px'
+    },
+    paper: {
+      padding: theme.spacing.unit,
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+      whiteSpace: 'nowrap',
+      marginBottom: theme.spacing.unit
+    },
+    centered: {
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      width: 'auto',
+      maxWidth: '800px',
+      minWidth: '300px'
+    },
+    divider: {
+      margin: `${theme.spacing.unit * 2}px 0`
+    },
+    banner: {
+      objectFit: 'cover',
+      width: '100%',
+      maxHeight: '90%',
+      height: 'calc(100vh - 55px)',
+      backgroundImage: 'url("./banner2.jpg")',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      color: 'white'
+    },
+    bannerText: {
+      color: 'white'
+    }
+  });
 
-  },
-  centered: {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    width: 'auto',
-    maxWidth: '800px',
-    minWidth: '300px'
-  },
-  divider: {
-    margin: `${theme.spacing.unit * 2}px 0`,
-  },
-});
-
-interface Props extends WithStyles<typeof styles> { }
-interface State { open: boolean }
+interface Props extends WithStyles<typeof styles> {}
+interface State {
+  open: boolean;
+}
 
 class Index extends React.Component<Props, State> {
   constructor(props: Props) {
-    super(props)
+    super(props);
     this.state = { open: false };
   }
 
   public render() {
     const { classes } = this.props;
     const { open } = this.state;
-
+// <img className={classes.banner} src="./banner2.jpg" />
     return (
-      <div>
-        <Typography variant="subheading" gutterBottom>
-          Material-UI Grid:
-      </Typography>
-      <div className={classes.centered}>
-        <Grid container spacing={24}>
-          <Grid item xs={12} md={12}>
-            <Paper className={classes.paper}>xs=3========|=|========</Paper>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Paper className={classes.paper}>xs=3</Paper>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Paper className={classes.paper}>xs=3</Paper>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Paper className={classes.paper}>xs=3</Paper>
-          </Grid>
-          <Grid item xs={12} md={8}>
-            <Paper className={classes.paper}>xs=8</Paper>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <Paper className={classes.paper}>xs=4</Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>xs=12</Paper>
-          </Grid>
-        </Grid>
+      <React.Fragment>
+        <div className={classes.banner}>
+          
+          <Typography variant="display1" gutterBottom align="center" className={classes.bannerText}>
+            Have a polite talk with your opposite family
+          </Typography>
+          
         </div>
-        <Divider className={classes.divider} />
-        <Typography variant="display1" gutterBottom>
-          Material-UI
-        </Typography>
-        <Typography variant="subheading" gutterBottom>
-          example project
-        </Typography>
-        <Button variant="contained" color="secondary" onClick={this.handleClick}>
-          Super Secret Password
-        </Button>
-      </div>
+
+        <div className={classes.centered}>
+          <Grid container spacing={24}>
+            <Grid item xs={12} md={12}>
+              <Paper className={classes.paper}>xs=12</Paper>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Paper className={classes.paper}>xs=3</Paper>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Paper className={classes.paper}>xs=3</Paper>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Paper className={classes.paper}>xs=3</Paper>
+            </Grid>
+            <Grid item xs={12} md={8}>
+              <Paper className={classes.paper}>xs=8</Paper>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Paper className={classes.paper}>xs=4</Paper>
+            </Grid>
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>xs=12</Paper>
+            </Grid>
+          </Grid>
+
+          <Divider className={classes.divider} />
+          <Typography variant="display1" gutterBottom>
+            Material-UI
+          </Typography>
+          <Typography variant="subheading" gutterBottom>
+            example project
+          </Typography>
+          <Button variant="contained" color="secondary" onClick={this.handleClick}>
+            Super Secret Password
+          </Button>
+        </div>
+      </React.Fragment>
     );
   }
 
   private handleClose = () => {
     this.setState({
-      open: false,
+      open: false
     });
   };
 
   private handleClick = () => {
     this.setState({
-      open: true,
+      open: true
     });
   };
 }
