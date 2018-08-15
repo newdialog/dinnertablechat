5ea2ff3dfe662ceced1b9ab3487d5d4c9e746ecf
Jonathan Dunlap <jdunlap@outlook.com>
2018-08-14 19:25:41 -0700

    add app bar
    Former-commit-id: a239b67b9a13e0fac48c4ea73d670fde22cbb550

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,16 @@ import * as ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-import Index from './pages/index';
+// import Index from './pages/index';
 // import AWSApp from './aws/AWSApp';
+import App from './App'
 
 // ReactDOM.render(<AWSApp />, document.querySelector('#root'));
 
 
 // import App from './App';
 ReactDOM.render(
-  <Index />,
+  <App />,
   document.getElementById('root') as HTMLElement
 );
 
