e722d2b63a371f8d5d52f54ac347eb303b0a12ba
Jonathan Dunlap <jdunlap@outlook.com>
2018-08-20 08:10:47 -0700

    wip router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Index from './components/home/index';
+import Index from './components/home/home';
 import AppBar from './components/AppBar';
 
 import { observer, inject } from 'mobx-react'
 
 const App = inject('store')(
   observer(({ store }) => {
+    console.log('page', store.router)
     return (<React.Fragment>
       <AppBar store={store} />
       <Index store={store} />
