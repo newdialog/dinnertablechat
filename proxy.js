d474fb29abc2e35cb05a4ee12d8d2c2b7ecca197
Jonathan Dunlap <jdunlap@outlook.com>
2018-08-22 09:46:44 -0700

    improve proxy logging and env ports

--- proxy.js
+++ proxy.js
@@ -1,9 +1,13 @@
 const fs = require('fs')
 var httpProxy = require('http-proxy');
+const SSL_PORT = process.env.SSL_PORT || 3003;
+const TO_PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3001;
+const goingTo = 'http://localhost:' + TO_PORT;
 
-console.log('listening on port ', PORT);
+console.log('listening on port:', SSL_PORT);
+console.log('https://localhost:'+SSL_PORT);
+console.log('proxy to ', goingTo)
  
 // var proxy = httpProxy.createProxyServer(options); // See (†)
 httpProxy.createServer({
@@ -11,7 +15,7 @@ httpProxy.createServer({
       key: fs.readFileSync('cert/key.pem', 'utf8'),
       cert: fs.readFileSync('cert/cert.pem', 'utf8')
     },
-    target: 'http://localhost:3000',
+    target: goingTo,
     secure: false, // Depends on your needs, could be false.
     protocolRewrite: 'https'
-  }).listen(PORT);
+  }).listen(SSL_PORT);
