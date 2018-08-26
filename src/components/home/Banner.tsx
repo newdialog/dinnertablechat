cf4f9ee74da2ea36ff88d379fe294350f61729d4
Jonathan Dunlap <jdunlap@outlook.com>
2018-08-26 12:00:07 -0700

    moved banner content into Banner component
    Former-commit-id: 996271fb9197d7126ea35d5c6e6228f18ed081e7

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -3,11 +3,11 @@ import { withStyles, createStyles, WithStyles } from '@material-ui/core/styles';
 import withRoot from '../../withRoot';
 
 import Lottie from 'react-lottie';
-import Content from './Content';
 import { observer } from 'mobx-react';
-import Subcribe from './Subscribe';
 
-import Banner from './Banner';
+import ArrowDown from '@material-ui/icons/KeyboardArrowDown';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
 
 const logoData = require('../../assets/logo.json');
 
@@ -66,14 +66,17 @@ const styles = (theme: any) =>
     bannerText: {
       fontFamily: 'Open Sans',
       color: 'white',
+      // position: 'absolute',
       bottom: '20%',
       marginBottom: '15vh',
       backgroundColor: '#00000044',
       fontWeight: 'bold'
+      // left: ''
     },
     logoanim: {
       width: '100vw',
       maxWidth: '600px',
+      // minHeight: '300px',
       marginLeft: 'auto',
       marginRight: 'auto',
       display: 'flex'
@@ -84,47 +87,36 @@ const styles = (theme: any) =>
     }
   });
 
-import AppModel from '../../models/AppModel';
 interface Props extends WithStyles<typeof styles> {
-  store: typeof AppModel.Type;
-}
-interface State {
-  open: boolean;
 }
 
 @observer
-class Index extends React.Component<Props, State> {
+class Index extends React.Component<Props> {
   constructor(props: Props) {
     super(props);
-    this.state = { open: false };
   }
 
   public render() {
     const { classes } = this.props;
-    const { open } = this.state;
     return (
       <React.Fragment>
-        <Banner/>
-
-        <Content />
-        <div className={classes.centered}>
-          <Subcribe />
+        <div className={classes.banner}>
+          <div className={classes.centeredDown}>
+            <Button href="#intro" size="small" variant="contained" color="primary">
+              Learn More
+            </Button>
+            <br />
+            <a href="#intro">
+              <IconButton style={{ height: '9em', width: '9em' }}>
+                <ArrowDown style={{ fontSize: '9em' }} />
+              </IconButton>
+              <div id="intro" style={{ height: 0 }} />
+            </a>
+          </div>
         </div>
       </React.Fragment>
     );
   }
-
-  private handleClose = () => {
-    this.setState({
-      open: false
-    });
-  };
-
-  private handleClick = () => {
-    this.setState({
-      open: true
-    });
-  };
 }
 
 export default withRoot(withStyles(styles)(Index));
