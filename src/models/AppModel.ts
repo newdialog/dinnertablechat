05542c5ab23c7efaee1225d8bb57069fdecb689b
Jonathan Dunlap <jdunlap@outlook.com>
2018-08-30 09:38:12 -0700

    switch to upstream mst-react-router
    Former-commit-id: 36a20efb453e1d085cbf38144e99e3eabf23ab6f

--- src/models/AppModel.ts
+++ src/models/AppModel.ts
@@ -1,23 +1,24 @@
-import { types } from 'mobx-state-tree'
-import { RouterModel } from '@jadbox/mst-react-router';
+import { types } from 'mobx-state-tree';
+import { RouterModel } from 'mst-react-router';
 
 const UserModel = types.model({
-    email: types.maybe(types.string),
-    name: types.string,
-    credits: types.integer,
-    karma: types.integer
-})
+  email: types.maybe(types.string),
+  name: types.string,
+  credits: types.integer,
+  karma: types.integer
+});
 
-const AppModel = types.model({
+const AppModel = types
+  .model({
     text: 'DEFAULT VAL',
     loggedIn: false,
     user: types.maybe(UserModel),
     router: RouterModel
-})
-.actions(self => ({
-    setTitle(newTitle:string) {
-        self.text = newTitle
+  })
+  .actions(self => ({
+    setTitle(newTitle: string) {
+      self.text = newTitle;
     }
-}))
+  }));
 
 export default AppModel;
