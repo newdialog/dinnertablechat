fe7d91b98366a952517146f7a9411b63111958af
Jonathan Dunlap <jdunlap@outlook.com>
2018-09-05 14:37:36 -0700

    content update, deps update, i18n start
    Former-commit-id: 58da8fd81f2ee6f2fd4fe196790455d100fff62b

--- src/services/QueueService.ts
+++ src/services/QueueService.ts
@@ -1,5 +1,47 @@
 import AWS from 'aws-sdk';
 
+let gameLift: AWS.GameLift;
+
+export function init() {
+  const options: AWS.GameLift.ClientConfiguration = {}
+  gameLift = new AWS.GameLift(options)
+}
+
+function onMatch(err: AWS.AWSError, data: AWS.GameLift.StartMatchmakingOutput) {
+  
+}
+
 export function queueUp() {
   console.log('queueUp');
+  const options: AWS.GameLift.StartMatchmakingInput = {
+    ConfigurationName: 'STRING_VALUE',
+    // TicketId: 'STRING_VALUE'
+    Players: [ /* required */
+      {
+        LatencyInMs: {
+          '<NonEmptyString>': 0,
+          /* '<NonEmptyString>': ... */
+        },
+        PlayerAttributes: {
+
+        },
+        PlayerId: 'STRING_VALUE',
+        Team: 'STRING_VALUE'
+      }
+    ]
+  }
+  gameLift.startMatchmaking(options, onMatch)
 }
+
+/*
+          '<NonZeroAndMaxString>': {
+            N: 0.0,
+            S: 'STRING_VALUE',
+            SDM: {
+              '<NonZeroAndMaxString>': 0.0,
+            },
+            SL: [
+              'STRING_VALUE',
+            ]
+          },
+*/
