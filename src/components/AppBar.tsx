3e2385ff09368625c3e3920c79c15dd240dc955d
Jonathan Dunlap <jdunlap@outlook.com>
2018-09-07 11:23:02 -0700

    polish
    Former-commit-id: 9a91e3a2af5e4f306788c3f70c8ba7498ce58d49

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -20,11 +20,7 @@ const styles = createStyles({
     flexGrow: 1,
     fontFamily: 'Montserrat!important',
     fontWeight: 'bolder'
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
+  }
 });
 
 interface Props extends WithStyles<typeof styles> {
@@ -32,7 +28,6 @@ interface Props extends WithStyles<typeof styles> {
 }
 
 function onLogin(store: Store.Type) {
-  // store.setTitle('clicked ' + Math.round(Math.random() * 10) )
   store.auth.login()
 
 }
@@ -48,25 +43,21 @@ function onStart(store: Store.Type) {
 function onHome(store: Store.Type) {
   store.router.push('/')
 }
-/*
-<IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-*/
 
 function ButtonAppBar(props:Props) {
   const { classes, store } = props;
   const auth = store.auth.loggedIn;
+
   return (
     <div className={classes.root}>
       <AppBar position="fixed" color="default" style={{ backgroundColor:'rgb(255,255,255,0.9)' }}>
         <Toolbar variant="dense">
           <img src="./logos/appbar-logo-color.png" style={{height:'3em', cursor: 'pointer'}} onClick={ onHome.bind(0,store) }/>
-          <Typography variant="title" color="inherit" className={classes.flex}>
-          </Typography>
+          { /* TODO BUG: empty component needed for alignment */ }
+          <Typography variant="title" className={classes.flex}></Typography>
 
           {!auth && 
-            <Button color="inherit" onClick={ () => onLogin(store) }>Login</Button>
+            <Button onClick={ () => onLogin(store) }>Login</Button>
           }
           {auth &&
             <React.Fragment>
@@ -74,7 +65,7 @@ function ButtonAppBar(props:Props) {
                 Start
                 <QueueIcon style={{marginLeft: '8px'}}></QueueIcon>
               </Button>
-              <Button color="inherit" onClick={ () => onLogOut(store) }>Log Out</Button>
+              <Button onClick={ () => onLogOut(store) }>Log Out</Button>
             </React.Fragment>
           }
 
--- src/models/AppModel.ts
+++ src/models/AppModel.ts
@@ -3,24 +3,16 @@ import { RouterModel } from 'mst-react-router';
 import AuthModel from './AuthModel';
 import { Instance } from 'mobx-state-tree';
 
-const AppModel = types
-  .model({
-    text: 'DEFAULT VAL',
-    auth: AuthModel,
-    router: RouterModel
-  })
-  .actions(self => ({
-    setTitle(newTitle: string) {
-      self.text = newTitle;
-    }
-  }));
+const AppModel = types.model({
+  auth: AuthModel,
+  router: RouterModel
+});
 
 export type Type = Instance<typeof AppModel>;
 
 export const create = (routerModel: RouterModel, fetcher: any) =>
   AppModel.create(
     {
-      text: 'DEFAULT VAL',
       router: routerModel,
       auth: AuthModel.create({})
     },
