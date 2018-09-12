9f3451f4e4436c5028c040ff8979000f91a0ce4a
Lauren Dunlap <laurenwdunlap@ucla.edu>
2018-09-11 17:06:32 -0700

    mock selection endpt integration

--- src/components/debate/DebateHome.tsx
+++ src/components/debate/DebateHome.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import { withStyles, createStyles, WithStyles, Theme } from '@material-ui/core/styles';
 import { AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Typography, Divider } from '@material-ui/core'
 import CameraIcon from '@material-ui/icons/PhotoCamera';
-
+import { data } from './sampleSelectionJSON';
 
 
 import withRoot from '../../withRoot';
@@ -12,29 +12,6 @@ import Lottie from 'react-lottie';
 import { observer } from 'mobx-react';
 
 // const logoData = require('../../assets/logo.json');
-
-// const styles = (theme: Theme) =>
-//   createStyles({
-//     root: {
-//       textAlign: 'center',
-//       paddingTop: theme.spacing.unit * 20
-//     },
-//     container: {
-//       display: 'grid',
-//       gridTemplateColumns: 'repeat(12, 1fr)',
-//       gridGap: `${theme.spacing.unit * 3}px`
-//       // gridAutoFlow: 'column',
-//       // gridAutoColumns: '200px'
-//     },
-//     centered: {
-//       marginTop: '60px',
-//       marginLeft: 'auto',
-//       marginRight: 'auto',
-//       width: 'auto',
-//       maxWidth: '800px',
-//       minWidth: '300px'
-//     },
-//   });
 
 const styles = theme => 
   createStyles({
@@ -84,9 +61,6 @@ const styles = theme =>
       padding: theme.spacing.unit * 6,
     },
   });
-
-// TODO: make it real, hardcoded collection of cards
-const cards = [1, 2, 3, 4];
 
 import * as AppModel from '../../models/AppModel';
 interface Props extends WithStyles<typeof styles> {
@@ -152,21 +126,18 @@ class Index extends React.Component<Props, State> {
         </div>
         <div className={classNames(classes.layout, classes.cardGrid)}>
           {/* End hero unit */}
-          <Grid container spacing={40}>
-            {cards.map(card => (
-              <Grid item key={card} sm={6} md={4} lg={3}>
+          <Grid container spacing={40} justify="center">
+            {data.map((card, i) => (
+              <Grid item key={i} sm={6} md={4} lg={3}>
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
-                    image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-                    title="Image title"
+                    image={card.photo}
+                    title={card.topic}
                   />
                   <CardContent className={classes.cardContent}>
                     <Typography gutterBottom variant="headline" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
+                      {card.topic}
                     </Typography>
                   </CardContent>
                   <CardActions>
@@ -185,12 +156,6 @@ class Index extends React.Component<Props, State> {
       </main>
       {/* Footer */}
       <footer className={classes.footer}>
-        <Typography variant="title" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography variant="subheading" align="center" color="textSecondary" component="p">
-          Something here to give the footer a purpose!
-        </Typography>
       </footer>
       {/* End footer */}
     </React.Fragment>
