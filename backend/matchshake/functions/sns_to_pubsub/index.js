da7fe96b5b73cac8e74263fff1eb25df00094b8c
Jonathan Dunlap <jdunlap@outlook.com>
2018-09-15 19:41:50 -0700

    matchshaker updates dynamo tables

--- backend/matchshake/functions/sns_to_pubsub/index.js
+++ backend/matchshake/functions/sns_to_pubsub/index.js
@@ -24,7 +24,7 @@ async function query() {
 }
 
 exports.handle = async (event, context, callback) => {
-  await query();
+  // await query();
   // console.log('Received event:', JSON.stringify(event, null, 4));
   // var message = JSON.parse(event.Records[0].Sns.Message);
   if (!event.Records || event.Records.length < 1 || !event.Records[0].Sns) {
@@ -52,8 +52,19 @@ function forEachSNS(record) {
 
   const ticketIds = tickets.map(t => t.ticketId);
 
-  console.log('Message received from SNS:', JSON.stringify(message));
-  console.log('tickets', ticketIds, 'players', JSON.stringify(players))
+  //console.log('Message received from SNS:', JSON.stringify(message));
+  console.log(matchId, 'tickets', ticketIds, 'players', JSON.stringify(players))
+
+  saveDB(players, matchId);
+}
+
+function saveDB(players, matchId) {
+  const params = getQParam(players, matchId)
+
+  docClient.batchWrite(params, function (err, data) {
+    if (err) console.warn('!!! ERROR', err);
+    // else console.log('updated db')
+  });
 }
 
 function getQParam(players, matchId) {
@@ -62,52 +73,40 @@ function getQParam(players, matchId) {
   // follower
   var blue = players.filter(p => p.team === 'blue')[0];
 
+  const ttl = Math.round((new Date).getTime() / 1000) + 60 * 3; // +3min
+  const ttl2 = Math.round((new Date).getTime() / 1000) + 60 * 60; // +60min
+
   return {
     RequestItems: {
       "match": [{
         PutRequest: {
           Item: {
-            "id": {
-              S: matchId
-            },
-            "redkey": {
-              S: ""
-            },
-            "bluekey": {
-              S: ""
-            }
+            "id": matchId,
+            "redkey": "-",
+            "bluekey": "-",
+            "ttl": '' + ttl2
           }
         }
       }],
       "dtc_sync": [{
           PutRequest: {
             Item: {
-              "user": {
-                S: red.playerId
-              },
-              "leader": {
-                BOOL: true
-              },
-              "team": "red",
-              "match": {
-                S: matchId
-              }
+              "user": '' + red.playerId,
+              "leader": true,
+              "team": 'red',
+              "match": matchId,
+              "ttl": '' + ttl
             }
           }
         },
         {
           PutRequest: {
             Item: {
-              "user": {
-                S: blue.playerId
-              },
-              "leader": {
-                BOOL: false
-              },
-              "team": "blue",
-              "match": {
-                S: matchId
-              }
+              "user": '' + blue.playerId,
+              "leader": false,
+              "team": 'blue',
+              "match": matchId,
+              "ttl": '' + ttl
             }
           }
         }
@@ -115,79 +114,79 @@ function getQParam(players, matchId) {
     }
   };
 }
-dynamodb.batchWriteItem(params, function (err, data) {
-
-      //======
-      // tickets [ '129116a4-d6d7-4fc4-b401-fefbf0615ac4','0ff741a3-6417-4c39-b7d7-1210cc62f714' ] 
-      // players [{"playerId":"p89","team":"red"},{"playerId":"p40","team":"blue"}]
-      /*
-      const message = {
-        "version": "0",
-        "id": "b925b8c8-a678-4311-802f-a51346310294",
-        "detail-type": "GameLift Matchmaking Event",
-        "source": "aws.gamelift",
-        "account": "681274315116",
-        "time": "2018-09-15T19:19:21.385Z",
-        "region": "us-east-1",
-        "resources": ["arn:aws:gamelift:us-east-1:681274315116:matchmakingconfiguration/dtc-match-config"],
-        "detail": {
-          "tickets": [{
-            "ticketId": "129116a4-d6d7-4fc4-b401-fefbf0615ac4",
-            "startTime": "2018-09-15T19:19:16.677Z",
-            "players": [{
-              "playerId": "p89",
-              "team": "red"
-            }]
-          }, {
-            "ticketId": "0ff741a3-6417-4c39-b7d7-1210cc62f714",
-            "startTime": "2018-09-15T19:19:21.283Z",
-            "players": [{
-              "playerId": "p40",
-              "team": "blue"
-            }]
-          }],
-          "ruleEvaluationMetrics": [{
-            "ruleName": "SameTopic",
-            "passedCount": 3,
-            "failedCount": 0
-          }, {
-            "ruleName": "DonationMatch",
-            "passedCount": 3,
-            "failedCount": 0
-          }, {
-            "ruleName": "DifferentSide",
-            "passedCount": 3,
-            "failedCount": 0
-          }],
-          "acceptanceRequired": false,
-          "type": "PotentialMatchCreated",
-          "gameSessionInfo": {
-            "players": [{
-              "playerId": "p89",
-              "team": "red"
-            }, {
-              "playerId": "p40",
-              "team": "blue"
-            }]
-          },
-          "matchId": "fd10a018-9053-4234-b890-8ed820317c3b"
-        }
-      }
 
 
-      const ticketsExample = [{
-          ticketId: 'e7e1b1eb-3021-470a-be69-83880b796bf0',
-          startTime: '2018-09-15T19:03:50.190Z',
-          players: [
-            [Object]
-          ]
-        },
-        {
-          ticketId: '3e1411f1-f160-4b46-8a1e-91a410d7778e',
-          startTime: '2018-09-15T19:03:52.504Z',
-          players: [
-            [Object]
-          ]
-        }
-      ]
-      */
+//======
+// tickets [ '129116a4-d6d7-4fc4-b401-fefbf0615ac4','0ff741a3-6417-4c39-b7d7-1210cc62f714' ] 
+// players [{"playerId":"p89","team":"red"},{"playerId":"p40","team":"blue"}]
+/*
+const message = {
+  "version": "0",
+  "id": "b925b8c8-a678-4311-802f-a51346310294",
+  "detail-type": "GameLift Matchmaking Event",
+  "source": "aws.gamelift",
+  "account": "681274315116",
+  "time": "2018-09-15T19:19:21.385Z",
+  "region": "us-east-1",
+  "resources": ["arn:aws:gamelift:us-east-1:681274315116:matchmakingconfiguration/dtc-match-config"],
+  "detail": {
+    "tickets": [{
+      "ticketId": "129116a4-d6d7-4fc4-b401-fefbf0615ac4",
+      "startTime": "2018-09-15T19:19:16.677Z",
+      "players": [{
+        "playerId": "p89",
+        "team": "red"
+      }]
+    }, {
+      "ticketId": "0ff741a3-6417-4c39-b7d7-1210cc62f714",
+      "startTime": "2018-09-15T19:19:21.283Z",
+      "players": [{
+        "playerId": "p40",
+        "team": "blue"
+      }]
+    }],
+    "ruleEvaluationMetrics": [{
+      "ruleName": "SameTopic",
+      "passedCount": 3,
+      "failedCount": 0
+    }, {
+      "ruleName": "DonationMatch",
+      "passedCount": 3,
+      "failedCount": 0
+    }, {
+      "ruleName": "DifferentSide",
+      "passedCount": 3,
+      "failedCount": 0
+    }],
+    "acceptanceRequired": false,
+    "type": "PotentialMatchCreated",
+    "gameSessionInfo": {
+      "players": [{
+        "playerId": "p89",
+        "team": "red"
+      }, {
+        "playerId": "p40",
+        "team": "blue"
+      }]
+    },
+    "matchId": "fd10a018-9053-4234-b890-8ed820317c3b"
+  }
+}
+
+
+const ticketsExample = [{
+    ticketId: 'e7e1b1eb-3021-470a-be69-83880b796bf0',
+    startTime: '2018-09-15T19:03:50.190Z',
+    players: [
+      [Object]
+    ]
+  },
+  {
+    ticketId: '3e1411f1-f160-4b46-8a1e-91a410d7778e',
+    startTime: '2018-09-15T19:03:52.504Z',
+    players: [
+      [Object]
+    ]
+  }
+]
+*/
