9db6a310f730473c27a6891b2dbae17ac9366f24
Jonathan Dunlap <jdunlap@outlook.com>
2018-09-17 12:38:24 -0700

    debate router update

--- src/components/debate/DebateRouter.tsx
+++ src/components/debate/DebateRouter.tsx
@@ -2,19 +2,26 @@ import * as React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import Tester from './DebateTester'
 import LoadingScene from './LoadingScene'
+import { observer, inject } from 'mobx-react';
+import withRoot from '../../withRoot';
 
+import * as AppModel from '../../models/AppModel';
+interface Props {
+  store: AppModel.Type;
+}
 
-const DTCRouter = ( {history}:{history:any} ) => (
-  <Router history={history}>
-    <Switch>
-      <Route exact={true} path="/" component={Tester} />
-      <Route path="/loading" component={LoadingScene} />
-
-    </Switch>
-  </Router>
-);
+const DebateRouter = ( props: Props ) => {
+  const store = props.store;
+  const ds = props.store.debate;
+  return (
+    <React.Fragment>
+      { ds.position === -1 && <Tester store={store}/> }
+      { ds.position > -1 && <LoadingScene store={store}/> }
+    </React.Fragment>
+  );
+};
 /*
 <Route path="/rtc" component={RTCHome}/>
 <DefaultRoute component={Home} />
 */
-export default DTCRouter
+export default inject('store')(withRoot(DebateRouter));
