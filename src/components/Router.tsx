9acca49c086d005ed9bb1aa82b0b0708a9dc9ef6
Jonathan Dunlap <jdunlap@outlook.com>
2018-09-17 17:15:39 -0700

    match state wip

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,23 +1,22 @@
 import * as React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import Home from './home/home'
 import DebateHome from './menus/MenuHome'
 import RTCHome from './debate/DebateScene'
 import LoadingMatch from './debate/DebateTester'
-// import DebateRouter from './debate/DebateRouter'
+import DebateRouter from './debate/DebateRouter'
 
 const DTCRouter = ( {history}:{history:any} ) => (
   <Router history={history}>
     <Switch>
       <Route exact={true} path="/" component={Home} />
-      <Route path="/callback" component={Home} />
-      <Route path="/signin" component={Home} />
-      <Route path="/signout" component={Home} />
-      
 
       <Route path="/play" component={DebateHome}/>
-      
-      <Route path="/match" component={LoadingMatch}/>
+      <Route path="/match" component={DebateRouter}/>
+
+      <Redirect from="/signin" to="/"/>
+      <Redirect from="/callback" to="/"/>
+      <Redirect from="/signout" to="/"/>
     </Switch>
   </Router>
 );
