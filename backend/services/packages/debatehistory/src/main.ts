78b409a322bd66ca3d721924c21133f1dab26ffb
Jonathan Dunlap <jdunlap@outlook.com>
2018-11-05 22:38:27 -0800

    lambda connection wip

--- backend/services/packages/debatehistory/src/main.ts
+++ backend/services/packages/debatehistory/src/main.ts
@@ -4,25 +4,39 @@ import * as express from 'express';
 
 // var AWSXRay = require('aws-xray-sdk');
 import bodyParser from 'body-parser';
-import { Client } from 'pg';
+import * as mysql from 'promise-mysql';
 
 const port = process.env.PORT || 8000;
+
+const sqloptions = {
+  host: process.env.SQL_HOST || 'localhost',
+  user: process.env.SQL_USER || 'me',
+  password: process.env.SQL_PW || 'secret',
+  database: 'dtc',
+};
+
 const app = express();
-
-
-const client = new Client()
-// await client.end()
-
+// const mysql = require('mysql');
+let connection: any;
 
 app.get('/hello', async (req: any, res: any, next: any) => {
-  const qres = await client.query('select * from "DebateSession"');
-  console.log(qres.rows) // Hello world!
+  if (!connection) throw new Error('db not init yet');
+  const qres = await connection.query('select * from debate_session');
+  console.log(qres.rows); // Hello world!
   res.send(qres.rows);
 });
 
 // app.use(bodyParser.json());
 // app.use(AWSXRay.express.closeSegment());
 app.listen(port, async () => {
-  await client.connect()
+  try {
+    connection = await mysql.createConnection(sqloptions);
+  } catch (e) {
+    throw new Error(e.toString());
+  }
+  // await connection.connect();
   console.log(`Live on port: ${port}!`);
+
+  sqloptions.password = 'removed';
+  console.log('sqloptions', sqloptions);
 });
