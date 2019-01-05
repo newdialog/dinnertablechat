c61a66a39a9b303785be8ed1b2e5869f8c08d4b4
Jonathan Dunlap <jdunlap@outlook.com>
2019-01-05 15:57:28 -0800

    refix icon

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -77,6 +77,7 @@ const DTCRouter = ({
 
         
         <Redirect from="/signout" to="/" />
+        <Redirect from="/CALLBACK" to="/callback" />
 
         <Route render={() => <Redirect to="/" />} />
       </Switch>
