08d33620483b6f1b7e47579bc00cc2d25186f321
Jonathan Dunlap <jdunlap@outlook.com>
2019-03-07 16:41:59 -0800

    position and char menu hooks

--- src/components/menus/CharacterSelection.tsx
+++ src/components/menus/CharacterSelection.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import { createStyles, WithStyles } from '@material-ui/core/styles';
+import { createStyles, WithStyles, Theme } from '@material-ui/core/styles';
 import { Button, Chip, Paper, Typography } from '@material-ui/core';
 import * as AppModel from '../../models/AppModel';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import HOC from '../HOC';
+import { useTranslation } from 'react-i18next';
+import { useTheme, makeStyles } from '@material-ui/styles';
 
 // requires user action
 function setLandscape() {
@@ -16,192 +18,166 @@ function setLandscape() {
 
   // const s = window.screen as any;
   const lockOrientationUniversal =
-    window.screen['lockOrientation'] || window.screen['mozLockOrientation'] || window.screen['msLockOrientation'];
-  
-    if (lockOrientationUniversal) {
-      if(lockOrientationUniversal.call(window.screen, 'landscape')) {
-        console.log('screen.lockOrientation set to landscape');
-      } else {
-        console.warn('screen.lockOrientation failed to lock');
-      }
+    window.screen['lockOrientation'] ||
+    window.screen['mozLockOrientation'] ||
+    window.screen['msLockOrientation'];
+
+  if (lockOrientationUniversal) {
+    if (lockOrientationUniversal.call(window.screen, 'landscape')) {
+      console.log('screen.lockOrientation set to landscape');
     } else {
-      console.warn('screen.lockOrientation not available')
+      console.warn('screen.lockOrientation failed to lock');
     }
+  } else {
+    console.warn('screen.lockOrientation not available');
+  }
 }
 
-const styles = theme =>
-  createStyles({
-    root: {
-      display: 'flex',
-      // justifyContent: 'center',
-      flexWrap: 'wrap',
-      padding: theme.spacing.unit / 2
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    display: 'flex',
+    // justifyContent: 'center',
+    flexWrap: 'wrap',
+    padding: theme.spacing.unit / 2
+  },
+  button: {
+    margin: theme.spacing.unit / 2
+  },
+  char: {
+    width: '20vw',
+    height: '20vw',
+    objectFit: 'contain'
+  },
+  image: {
+    position: 'relative',
+    width: '20vw',
+    height: '20vw',
+    // height: 200,
+    [theme.breakpoints.down('xs')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 150
     },
-    button: {
-      margin: theme.spacing.unit / 2
-    },
-    char: {
-      width: '20vw',
-      height: '20vw',
-      objectFit: 'contain'
-    },
-    image: {
-      position: 'relative',
-      width: '20vw',
-      height: '20vw',
-      // height: 200,
-      [theme.breakpoints.down('xs')]: {
-        width: '100% !important', // Overrides inline-style
-        height: 150,
+    '&:hover, &$focusVisible': {
+      zIndex: 1,
+      '& $imageBackdrop': {
+        opacity: 0.15
       },
-      '&:hover, &$focusVisible': {
-        zIndex: 1,
-        '& $imageBackdrop': {
-          opacity: 0.15,
-        },
-        '& $imageMarked': {
-          opacity: 0,
-        },
-        '& $imageTitle': {
-          border: '4px solid currentColor',
-        },
+      '& $imageMarked': {
+        opacity: 0
       },
-    },
-    focusVisible: {},
-    imageButton: {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      top: 'auto',
-      bottom: '.25em',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      color: theme.palette.common.white,
-    },
-    imageSrc: {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center 40%',
-    },
-    imageBackdrop: {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-      backgroundColor: theme.palette.common.black,
-      opacity: 0.4,
-      transition: theme.transitions.create('opacity'),
-    },
-    imageTitle: {
-      position: 'relative',
-      padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 4}px ${theme.spacing.unit + 6}px`,
-    },
-    imageMarked: {
-      height: 3,
-      width: 18,
-      backgroundColor: theme.palette.common.white,
-      position: 'absolute',
-      bottom: -2,
-      left: 'calc(50% - 9px)',
-      transition: theme.transitions.create('opacity'),
+      '& $imageTitle': {
+        border: '4px solid currentColor'
+      }
     }
-  });
+  },
+  focusVisible: {},
+  imageButton: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 'auto',
+    bottom: '.25em',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.white
+  },
+  imageSrc: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center 40%'
+  },
+  imageBackdrop: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: theme.palette.common.black,
+    opacity: 0.4,
+    transition: theme.transitions.create('opacity')
+  },
+  imageTitle: {
+    position: 'relative',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 4}px ${theme
+      .spacing.unit + 6}px`
+  },
+  imageMarked: {
+    height: 3,
+    width: 18,
+    backgroundColor: theme.palette.common.white,
+    position: 'absolute',
+    bottom: -2,
+    left: 'calc(50% - 9px)',
+    transition: theme.transitions.create('opacity')
+  }
+}));
 
-interface Props extends WithStyles<typeof styles> {
+interface Props {
   store: AppModel.Type;
 }
 
- 
-class CharacterSelector extends React.Component<Props> {
-  private defaultContributions(): Array<{
-    key: any;
-    title: any;
-    value: any;
-    url: any;
-  }> {
-    return [
-      { key: 0, title: 'Tracy', value: 0, url: './imgs/04-select.png' },
-      { key: 1, title: 'Riley', value: 1, url: './imgs/04-select2.png' },
-      { key: 2, title: 'Finley', value: 2, url: './imgs/04-select3.png' }
-    ];
-  }
+const defaultContributions: Array<{
+  key: any;
+  title: any;
+  value: any;
+  url: any;
+}> = [
+  { key: 0, title: 'Tracy', value: 0, url: './imgs/04-select.png' },
+  { key: 1, title: 'Riley', value: 1, url: './imgs/04-select2.png' },
+  { key: 2, title: 'Finley', value: 2, url: './imgs/04-select3.png' }
+];
 
-  private onChipClick(ev:any, item: any) {
+export default function CharacterSelector(props: Props) {
+  const store = props.store;
+  const classes = useStyles({});
+  const { t } = useTranslation();
+  const onChipClick = (ev: any, item: any) => {
     setLandscape();
-    this.props.store.debate.setCharacter(item.value);
-  }
+    props.store.debate.setCharacter(item.value);
+  };
 
-  public render() {
-    const { classes, store } = this.props;
-    // const selected = store.debate.character;
+  const contrib = defaultContributions;
 
-    const contrib = this.defaultContributions();
-    /* if(selected!==-1) {
-        contrib.forEach( (e) => {
-            if(e.label=== selected) e.color = 'primary';
-        })
-    }*/
-
-    return (
-      <div className={classes.root}>
-          {contrib.map(item => {
-            return (
-              <ButtonBase
-                focusRipple
-                key={item.title}
-                className={classes.image}
-                focusVisibleClassName={classes.focusVisible}
-                style={{
-                  width: '30%'
-                }}
-                onClick={(e) => this.onChipClick(e, item)}
-              >
-                <span
-                  className={classes.imageSrc}
-                  style={{
-                    backgroundImage: `url(${item.url})`
-                  }}
-                />
-                <span className={classes.imageBackdrop} />
-                <span className={classes.imageButton}>
-                  <Typography
-                    component="span"
-                    variant="subtitle1"
-                    color="inherit"
-                    className={classes.imageTitle}
-                  >
-                    {item.title}
-                    <span className={classes.imageMarked} />
-                  </Typography>
-                </span>
-              </ButtonBase>
-            );
-          })}
-        </div>
-    );
-  }
-}
-{
-  /* 
-  <Chip
-                key={item.key}
-                label={item.label}
-                color={item.color}
-                onClick={() => this.onChipClick(item)}
-                className={classes.button}
-                clickable
+  return (
+    <div className={classes.root}>
+      {contrib.map(item => {
+        return (
+          <ButtonBase
+            focusRipple
+            key={item.title}
+            className={classes.image}
+            focusVisibleClassName={classes.focusVisible}
+            style={{
+              width: '30%'
+            }}
+            onClick={e => onChipClick(e, item)}
+          >
+            <span
+              className={classes.imageSrc}
+              style={{
+                backgroundImage: `url(${item.url})`
+              }}
             />
-  
-  <Button key={item.key} variant="outlined" size="small" color="primary" 
-className={classes.button} onClick={() => this.onChipClick(item)}>
-{item.label.valueOf()}
-</Button> */
+            <span className={classes.imageBackdrop} />
+            <span className={classes.imageButton}>
+              <Typography
+                component="span"
+                variant="subtitle1"
+                color="inherit"
+                className={classes.imageTitle}
+              >
+                {item.title}
+                <span className={classes.imageMarked} />
+              </Typography>
+            </span>
+          </ButtonBase>
+        );
+      })}
+    </div>
+  );
 }
-
-export default HOC(CharacterSelector, styles);
--- src/components/menus/MenuHome.tsx
+++ src/components/menus/MenuHome.tsx
@@ -1,6 +1,14 @@
-import * as React from 'react';
-import { createStyles, WithStyles } from '@material-ui/core/styles';
-import { Button, Typography, Stepper, Step, StepLabel, StepContent, Paper } from '@material-ui/core'
+import React, { useState, useEffect, useContext } from 'react';
+import { createStyles, WithStyles, Theme } from '@material-ui/core/styles';
+import {
+  Button,
+  Typography,
+  Stepper,
+  Step,
+  StepLabel,
+  StepContent,
+  Paper
+} from '@material-ui/core';
 import * as AppModel from '../../models/AppModel';
 import PositionSelector from './PositionSelector';
 import ContributionSelector from './ContributionSelector';
@@ -10,66 +18,63 @@ import HOC from '../HOC';
 import Footer from '../home/Footer';
 import HistoryIcon from '@material-ui/icons/History';
 import * as Times from '../../services/TimeService';
+import { observer } from 'mobx-react-lite';
+import { useTranslation } from 'react-i18next';
+import { useTheme, makeStyles } from '@material-ui/styles';
 
-const styles = theme => 
-  createStyles({
-    pagebody: {
-      backgroundColor:theme.palette.primary.light,
-      minHeight: '100vh'
-    },
-    container: {
-      marginTop: '0px',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      width: 'auto',
-      maxWidth: '1000px',
-      minWidth: '300px'
-    },
-    appBar: {
-      position: 'relative',
-    },
-    icon: {
-      marginRight: theme.spacing.unit * 2,
-    },
-    heroUnit: {
-      // backgroundColor: theme.palette.background.paper,
-    },
-    heroContent: {
-      maxWidth: 600,
-      textAlign:'center',
-      margin: '0 auto',
-      padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 0}px`,
-    },
-    stepper: {
-      padding: theme.spacing.unit * 0,
-    },
-    button: {
-      marginTop: theme.spacing.unit,
-      marginRight: theme.spacing.unit,
-      color: theme.palette.primary.dark
-    },
-    actionsContainer: {
-      marginBottom: theme.spacing.unit * 2,
-    },
-    resetContainer: {
-      padding: theme.spacing.unit * 3,
-    },
-    footer: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing.unit * 6,
-    },
-    linkhome: {
-      color: theme.palette.primary.dark
-    }
-  });
+const useStyles = makeStyles((theme: Theme) => ({
+  pagebody: {
+    backgroundColor: theme.palette.primary.light,
+    minHeight: '100vh'
+  },
+  container: {
+    marginTop: '0px',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: 'auto',
+    maxWidth: '1000px',
+    minWidth: '300px'
+  },
+  appBar: {
+    position: 'relative'
+  },
+  icon: {
+    marginRight: theme.spacing.unit * 2
+  },
+  heroUnit: {
+    // backgroundColor: theme.palette.background.paper,
+  },
+  heroContent: {
+    maxWidth: 600,
+    textAlign: 'center',
+    margin: '0 auto',
+    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 0}px`
+  },
+  stepper: {
+    padding: theme.spacing.unit * 0
+  },
+  button: {
+    marginTop: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    color: theme.palette.primary.dark
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing.unit * 2
+  },
+  resetContainer: {
+    padding: theme.spacing.unit * 3
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing.unit * 6
+  },
+  linkhome: {
+    color: theme.palette.primary.dark
+  }
+}));
 
-interface Props extends WithStyles<typeof styles> {
-  store: AppModel.Type;
+interface Props {
   isTest?: boolean;
-}
-interface State {
-  open: boolean;
-  activeStep: number,
 }
 
 function getSteps() {
@@ -83,83 +88,81 @@ function onHistory(store: AppModel.Type) {
 function getStepContent(step: number, store: AppModel.Type) {
   switch (step) {
     case 0:
-      return <CharacterSelection store={store} />
+      return <CharacterSelection store={store} />;
     case 1:
-      return <PositionSelector store={store} />
+      return <PositionSelector store={store} />;
     case 2:
-      return <ContributionSelector store={store} />
+      return <ContributionSelector store={store} />;
     default:
-      return <Typography>Hmm, something went wrong. Please try again after refreshing the page.</Typography>;
+      return (
+        <Typography>
+          Hmm, something went wrong. Please try again after refreshing the page.
+        </Typography>
+      );
   }
 }
 
- 
-class Index extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = { open: false, activeStep: 0 };
+const renderStepButtons = (activeStep, classes, handleBack) => {
+  return (
+    <div className={classes.actionsContainer}>
+      <Button
+        disabled={activeStep === 0}
+        onClick={handleBack}
+        className={classes.button}
+        color="secondary"
+      >
+        Reset Selections
+      </Button>
+    </div>
+  );
+};
 
-    localStorage.removeItem('quickmatch');
-  }
+export default observer(function MenuHome(props: Props) {
+  const store = useContext(AppModel.Context)!;
+  const classes = useStyles({});
+  const { t } = useTranslation();
+  const [state, setState] = React.useState({ open: false, activeStep: 0 });
 
-  public componentDidMount() {
-    if(this.props.store.isLive() && !Times.isDuringDebate()) {
-      this.props.store.router.push('/home');
+  useEffect(() => localStorage.removeItem('quickmatch'), []);
+
+  useEffect(() => {
+    if (store.isLive() && !Times.isDuringDebate()) {
+      store.router.push('/home');
     }
-    if(Boolean(this.props.isTest) !== this.props.store.debate.isTest) { 
-      this.props.store.debate.setTest(this.props.isTest===true);
+    if (Boolean(props.isTest) !== store.debate.isTest) {
+      store.debate.setTest(props.isTest === true);
     }
-    this.handleReset();
+    handleReset();
 
     window.gtag('event', 'debate_match_menu', {
       event_category: 'splash',
-      guest: this.props.store.isGuest()
+      guest: store.isGuest()
     });
-  }
+  }, []);
 
-  private handleBack = () => {
-    this.handleReset();
+  const handleBack = () => {
+    handleReset();
   };
 
-  private handleReset = () => {
-    const { store } = this.props;
+  const handleReset = () => {
     store.debate.resetQueue();
   };
 
-  private renderStepButtons = (activeStep, classes, steps) => {
-    return (
-      <div className={classes.actionsContainer}>
-        <Button
-          disabled={activeStep === 0}
-          onClick={this.handleBack}
-          className={classes.button}
-          color="secondary"
-        >
-          Reset Selections
-        </Button>
-      </div>
-    );
+  if (store.auth.isNotLoggedIn) {
+    store.router.push('/');
+    return <div />;
   }
+  let step = 3;
 
+  if (store.debate.contribution === -1) step = 2; // skip contribution
+  if (!store.debate.topic || store.debate.position === -1) step = 1;
+  if (store.debate.character === -1) step = 0;
 
-  public render() {
-    const { classes, store } = this.props;
-    if(store.auth.isNotLoggedIn) {
-      store.router.push('/');
-      return (<div/>);
-    }
-    let step = 3;
-    
-    if(store.debate.contribution===-1) step = 2; // skip contribution
-    
-    if(!store.debate.topic || store.debate.position===-1) step = 1;
-    if(store.debate.character===-1) step = 0;
-    
-    // if(store.debate.position !== -1 && store.debate.contribution !== -1) step = 2;
-    // if(store.debate.character !== -1) step = 3;
-    if(step===3) store.router.push('/match');
-    // console.log('step', step)
-    const steps = getSteps();
+  // if(store.debate.position !== -1 && store.debate.contribution !== -1) step = 2;
+  // if(store.debate.character !== -1) step = 3;
+  if (step === 3) store.router.push('/match');
+  // console.log('step', step)
+  const steps = getSteps();
 
   return (
     <div className={classes.pagebody}>
@@ -167,22 +170,35 @@ class Index extends React.Component<Props, State> {
         {/* Hero unit */}
         <div className={classes.heroUnit}>
           <div className={classes.heroContent}>
-            { this.props.isTest && (<h2>TEST MODE (/test)</h2>)}
-            <Typography style={{fontSize: '3em', paddingBottom: '0', color: '#ffffff'}} variant="h3" align="center" color="textSecondary" gutterBottom>
+            {props.isTest && <h2>TEST MODE (/test)</h2>}
+            <Typography
+              style={{ fontSize: '3em', paddingBottom: '0', color: '#ffffff' }}
+              variant="h3"
+              align="center"
+              color="textSecondary"
+              gutterBottom
+            >
               Debate Quickmatch
             </Typography>
           </div>
         </div>
         {/* End hero unit */}
         <div className={classes.stepper}>
-          <Stepper color='primary' activeStep={step} orientation="vertical" style={{backgroundColor:'#2db8cc'}}>
+          <Stepper
+            color="primary"
+            activeStep={step}
+            orientation="vertical"
+            style={{ backgroundColor: '#2db8cc' }}
+          >
             {steps.map((label, index) => {
               return (
                 <Step key={label}>
                   <StepLabel>{label}</StepLabel>
                   <StepContent>
                     {getStepContent(index, store)}
-                    {(step === 0) ? null : this.renderStepButtons(step, classes, steps)}
+                    {step === 0
+                      ? null
+                      : renderStepButtons(step, classes, handleBack)}
                   </StepContent>
                 </Step>
               );
@@ -190,24 +206,33 @@ class Index extends React.Component<Props, State> {
           </Stepper>
           {step > steps.length && (
             <Paper square elevation={0} className={classes.resetContainer}>
-              <Typography>All steps completed - you&quot;re about to enter the queue for {store.debate.topic}!</Typography>
-              <Button color="secondary" onClick={this.handleReset} className={classes.button}>
+              <Typography>
+                All steps completed - you&quot;re about to enter the queue for{' '}
+                {store.debate.topic}!
+              </Typography>
+              <Button
+                color="secondary"
+                onClick={handleReset}
+                className={classes.button}
+              >
                 Reset
               </Button>
             </Paper>
           )}
         </div>
 
-        <Button className={classes.linkhome} color="secondary" onClick={ () => onHistory(store) }>
+        <Button
+          className={classes.linkhome}
+          color="secondary"
+          onClick={() => onHistory(store)}
+        >
           Back to Profile Home
-        <HistoryIcon style={{marginLeft: '8px'}}></HistoryIcon>
+          <HistoryIcon style={{ marginLeft: '8px' }} />
         </Button>
       </main>
     </div>
   );
-  }
-}
-export default inject('store')(HOC(Index, styles));
+});
 
 // took this out as height is a little wierd on page
 // <Footer className={classes.footer}/>
